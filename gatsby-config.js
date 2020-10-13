c3ed58131b8053537446a10f7aa4f72d55273847
Kaiguang Li <33639861+Kaiguang@users.noreply.github.com>
2020-10-13 00:54:55 -0600

    Add plugin to deploy to AWS S3

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,15 @@ module.exports = {
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "likaiguang.com",
+        acl: null,
+        protocol: `https`,
+        hostname: `likaiguang.com`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
