52e25968448f4e32130b5f463477cbdc3035d379
Kaiguang Li <33639861+Kaiguang@users.noreply.github.com>
2020-10-16 10:53:30 -0600

    Fix page js file case insensitivity

---
+++ src/pages/blog.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout.js";
+import styles from "./blog.module.css";
+
+export default function Blog({ data }) {
+  return (
+    <Layout selectedNavLink="blog">
+      <div className={styles.blogList}>
+        {data.allMarkdownRemark.nodes.map(({ frontmatter, fields }) => (
+          <Link
+            key={fields.slug}
+            to={`/blog${fields.slug}`}
+            id={fields.slug.substr(1)}
+            className={styles.blogLink}
+          >
+            <div className={styles.blogTitle}>
+              <span>{frontmatter.title}</span>
+            </div>
+            <div className={styles.blogTagsAndDate}>
+              <div>
+                {frontmatter.tags.map((tag) => (
+                  <span key={tag} className={styles.blogTag}>
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <span className={styles.blogTag}>{frontmatter.date}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "YYYY-MM-DD")
+          tags
+        }
+        fields {
+          slug
+        }
+      }
+    }
+  }
+`;
---
+++ src/pages/contact.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import Layout from "../components/Layout.js";
+import styles from "./Contact.module.css";
+
+export default function Contact() {
+  const [myNameTooltipText, setMyNameTooltipText] = useState("Click to copy");
+
+  const handleCopyMyNameClick = () => {
+    const textArea = document.createElement("textarea");
+    textArea.value = "Kaiguang Li";
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    textArea.remove();
+    setMyNameTooltipText("Copied");
+  };
+
+  const handleMyNameMouseLeave = () => {
+    setMyNameTooltipText("Click to copy");
+  };
+
+  const [myEmailTooltipText, setMyEmailTooltipText] = useState("Click to copy");
+
+  const handleCopyMyEmailClick = () => {
+    const textArea = document.createElement("textarea");
+    textArea.value = "lkaiguang@gmail.com";
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    textArea.remove();
+    setMyEmailTooltipText("Copied");
+  };
+
+  const handleMyEmailMouseLeave = () => {
+    setMyEmailTooltipText("Click to copy");
+  };
+
+  return (
+    <Layout selectedNavLink="contact">
+      <div className={styles.contactContainer}>
+        <code>{`---`}</code>
+        <code>
+          Name:{" "}
+          <span
+            className={styles.hoverAndTooltip}
+            onClick={handleCopyMyNameClick}
+            onKeyPress={handleCopyMyNameClick}
+            onMouseLeave={handleMyNameMouseLeave}
+            role="button"
+            tabIndex="0"
+          >
+            Kaiguang Li
+            <span className={styles.hoverAndTooltipTooltipText}>
+              {myNameTooltipText}
+            </span>
+          </span>
+        </code>
+        <code>
+          Email:{" "}
+          <span
+            className={styles.hoverAndTooltip}
+            onClick={handleCopyMyEmailClick}
+            onKeyPress={handleCopyMyEmailClick}
+            onMouseLeave={handleMyEmailMouseLeave}
+            role="button"
+            tabIndex="0"
+          >
+            lkaiguang@gmail.com
+            <span className={styles.hoverAndTooltipTooltipText}>
+              {myEmailTooltipText}
+            </span>
+          </span>
+        </code>
+        <code>
+          GitHub:{" "}
+          <a
+            href="https://github.com/Kaiguang"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Kaiguang
+          </a>
+        </code>
+        <code>
+          LinkedIn:{" "}
+          <a
+            href="https://www.linkedin.com/in/kaiguang-li/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            kaiguang-li
+          </a>
+        </code>
+        <code>
+          Twitter:{" "}
+          <a
+            href="https://twitter.com/KaiguangLi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @KaiguangLi
+          </a>
+        </code>
+        <code>{`---`}</code>
+        <div className={styles.yawd}>
+          <p>kai.yawd</p>
+          <p>Yet Another Web Developer</p>
+        </div>
+      </div>
+    </Layout>
+  );
+}
