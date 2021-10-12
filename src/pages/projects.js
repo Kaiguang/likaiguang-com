da16beb19a74623fc62232e6efa3881eeb67feea
Kaiguang Li <33639861+Kaiguang@users.noreply.github.com>
2021-10-11 23:22:16 -0600

    Fix typo

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
-import * as styles from './Projects.module.css'
+import * as styles from './projects.module.css'
 
 export default function Projects() {
   return (
