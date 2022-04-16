6be56470e40eeaf20b4772752c1e3293f7c111f5
Kaiguang Li <33639861+Kaiguang@users.noreply.github.com>
2022-04-16 12:26:30 -0600

    Fix NavBar to show active links for static pages

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import { Link as GatsbyLink } from 'gatsby';
+import { useLocation } from '@reach/router';
 import { Logo } from './Logo';
 import { BookIcon } from './BookIcon';
 
@@ -19,12 +20,16 @@ export function NavBar() {
 }
 
 function NavBarLink({ path, children }: { path: string; children: ReactNode }) {
+  const { pathname } = useLocation();
+  const isMatch = new RegExp(`^${path}`, 'gi').test(pathname);
+  const className =
+    `flex items-end text-lg font-light h-10 ml-1 sm:ml-2 px-2 pb-[2px] ` +
+    `border-0 rounded-md border-transparent ` +
+    `hover:bg-kaibluelight hover:text-gray-100 hover:fill-gray-100 ` +
+    (isMatch ? `bg-gradient-to-b from-kaiblue to-kaibluelight text-gray-100 fill-gray-100` : '');
+
   return (
-    <GatsbyLink
-      to={path}
-      className='flex items-end text-lg font-light h-10 ml-1 sm:ml-2 px-2 pb-[2px] border-0 rounded-md border-transparent hover:bg-kaibluelight hover:text-gray-100 hover:fill-gray-100'
-      activeClassName='bg-gradient-to-b from-kaiblue to-kaibluelight text-gray-100 fill-gray-100'
-    >
+    <GatsbyLink to={path} className={className}>
       {children}
     </GatsbyLink>
   );
